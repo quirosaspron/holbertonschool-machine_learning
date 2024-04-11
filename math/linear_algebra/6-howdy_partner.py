c2fcf88068ab7f5faaf5c8e7329bcedb31298340
Mateo <mateoquirosaspron@gmail.com>
2024-04-10 21:42:14 -0600

    -

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+""" Concatenates two arrays """
+
+
 def cat_arrays(arr1, arr2):
+    """Concatenates two arrays"""
     new_arr = arr1
     for element in arr2:
         new_arr.append(element)
