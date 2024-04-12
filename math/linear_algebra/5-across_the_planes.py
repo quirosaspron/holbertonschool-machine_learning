45da1350d4b15ea41460e007bde48373321f1af9
Mateo <mateoquirosaspron@gmail.com>
2024-04-11 19:20:39 -0600

    -

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -7,7 +7,7 @@ def add_matrices2D(mat1, mat2):
     matrix_shape = __import__('2-size_me_please').matrix_shape
     if matrix_shape(mat1) != matrix_shape(mat2):
         return None
-    else:
+    elif matrix_shape(mat1) == matrix_shape(mat2):
         new_matrix = [[] for row in mat1]
         r = 0
         for row in mat1:
@@ -17,3 +17,5 @@ def add_matrices2D(mat1, mat2):
                 i += 1
             r += 1
         return new_matrix
+    else:
+        return None
