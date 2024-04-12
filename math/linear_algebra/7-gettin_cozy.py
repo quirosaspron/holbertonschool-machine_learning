373adeb7e2b414f366570490821e75c1e6681065
Mateo <mateoquirosaspron@gmail.com>
2024-04-11 19:18:10 -0600

    -

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -15,11 +15,3 @@ def cat_matrices2D(mat1, mat2, axis=0):
         return new_mat
     else:
         return None
-
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6]]
-mat3 = [[7], [8]]
-mat4 = cat_matrices2D(mat1, mat2)
-mat5 = cat_matrices2D(mat1, mat3, axis=1)
-print(mat4)
-print(mat5)
