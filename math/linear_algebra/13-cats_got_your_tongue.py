75923eda1dc93a9743f484b4c02122cba7aaef9e
Mateo <mateoquirosaspron@gmail.com>
2024-04-12 18:04:44 -0600

    -

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -2,6 +2,7 @@
 """Concatenates two matrices along a specific axis"""
 
 
+import numpy as np
 def np_cat(mat1, mat2, axis=0):
     """Returns the matrices concatenated"""
     return np.concatenate((mat1, mat2), axis)
