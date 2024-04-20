145a6293259a1eb10903e3aa154887ffebfd6405
Mateo <mateoquirosaspron@gmail.com>
2024-04-20 17:18:08 -0600

    -

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -3,18 +3,22 @@
 
 
 def poly_integral(poly, C=0):
-    """Integrates a polynomial"""
-    if not isinstance(poly, list) or not all(isinstance(coeff, (int, float)) for coeff in poly) or not isinstance(C, int):
+    """function to integrate a poly"""
+
+    if type(poly) is not list:
         return None
-    if len(poly) == 0:
+    elif not poly:
         return None
-    integral_coeffs = [C]
-    for i in range(len(poly)):
-        if i == 0:
-            integral_coeffs.append(poly[i] / (i + 1))
-        else:
-            integral_coeffs.append(poly[i] / (i + 1))
-    while integral_coeffs[-1] == 0 and len(integral_coeffs) > 1:
-        integral_coeffs.pop()
-    
-    return integral_coeffs
+    elif len(poly) == 0:
+        return None
+    elif type(C) is not int:
+        return None
+    elif poly == [0]:
+        return [C]
+    else:
+        integral = []
+        integral.append(C)
+        for i in range(len(poly)):
+            x = poly[i] / (i + 1)
+            integral.append(int(x) if x.is_integer() else x)
+        return integral
