1271d8b5ef015861400b687bee2c7ec519c01bb4
Mateo <mateoquirosaspron@gmail.com>
2024-04-20 12:19:45 -0600

    -

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Calculates the derivative of a polynomial"""
+
+
+def poly_derivative(poly):
+    """Derives a polynomial"""
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+    else:
+        poly[0] = 0
+        for i, x in enumerate(poly):
+            if not isinstance(x, int):
+                return None
+            elif i > 0:
+                poly[i-1] = i*x
+        poly.pop()
+        return poly
