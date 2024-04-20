ba968940ea62a2f201866a8891b4e699e9282900
Mateo <mateoquirosaspron@gmail.com>
2024-04-20 11:48:33 -0600

    -

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,7 +4,9 @@
 
 def summation_i_squared(n):
     """Sums i squared n times"""
-    if not n >= 1:
-        return none
+    if not n <= 1:
+        return None
     else:
         return int((n*(n+1)*(2*n+1))/6)
+
+print(summation_i_squared(0.24))
