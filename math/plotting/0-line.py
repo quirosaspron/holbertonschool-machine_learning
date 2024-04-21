839eb0636638838b2a1dccb0c18ffdfa926cff5e
Mateo <mateoquirosaspron@gmail.com>
2024-04-21 16:55:56 -0600

    -

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+"""Plots a red line"""
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-y = np.arange(0, 11) ** 3
 
-
-plt.xlim(0, 10)
-plt.plot(y, color='red')
-plt.show()
+def line():
+    "Plots a red line"""
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    plt.xlim(0, 10)
+    plt.plot(y, color='red')
+    plt.show
