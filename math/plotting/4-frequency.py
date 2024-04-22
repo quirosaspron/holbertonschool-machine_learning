a0ecbbcb0676567be5e928cc6dff235fad410da6
Mateo <mateoquirosaspron@gmail.com>
2024-04-21 23:02:27 -0600

    -

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -8,14 +8,14 @@ def frequency():
     "Plots student scores for a project"
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-    plt.figure(figsize=(6.4, 4.8))
-    # Plot histogram
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
 
-    # Set labels and title
+    plt.hist(student_grades,
+         bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+         edgecolor='black')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
-    plt.title('Project A')
-
-    # Show plot
+    plt.title("Project A")
+    plt.xlim((0, 100))
+    plt.xticks(np.arange(0, 101, 10))
+    plt.ylim((0, 30))
     plt.show()
