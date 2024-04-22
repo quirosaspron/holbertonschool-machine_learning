6424633acdc970be98544599d658f69f6772ab23
Mateo <mateoquirosaspron@gmail.com>
2024-04-21 22:53:06 -0600

    -

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Plots a stacked bar graph"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def bars():
+    "Plots a bar graph"
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4,3))
+    plt.figure(figsize=(6.4, 4.8))
+    rows = ('apples', 'bananas', 'oranges', 'peaches')
+    columns = ('Farrah', 'Fred', 'Felicia')
+    index = columns
+    colors = ('red', 'yellow', '#ff8000', '#ffe5b4')
+    n_rows = len(fruit)
+    bar_width = 0.5
+    y_offset = np.zeros(len(columns))
+
+
+   for row in range(n_rows):
+       plt.bar(index, fruit[row], bar_width, bottom=y_offset,
+              color=colors[row], label=rows[row])
+       y_offset = y_offset + fruit[row]
+
+
+    plt.legend()
+    plt.yticks(np.arange(0, 90, 10))
+    plt.ylabel('Quantity of Fruit')
+    plt.title("Number of Fruit per Person")
+    plt.show()
