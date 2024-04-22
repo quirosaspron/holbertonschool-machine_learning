f541ef118ec574a591169cf05a4d2f367ef3e2e1
Mateo <mateoquirosaspron@gmail.com>
2024-04-21 23:05:10 -0600

    -

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -9,9 +9,8 @@ def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
 
-    plt.hist(student_grades,
-         bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-         edgecolor='black')
+    bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    plt.hist(student_grades, bins, edgecolor='black')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title("Project A")
