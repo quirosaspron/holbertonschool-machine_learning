51617bf8bc6e943af97dab96d30e4fa7c3141520
Mateo <mateoquirosaspron@gmail.com>
2024-04-28 13:19:32 -0600

    -

--- math/plotting/100-gradient.py
+++ math/plotting/100-gradient.py
@@ -3,15 +3,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
+def gradient():
+    """Plots a scatter plot"""
+    np.random.seed(5)
 
-x = np.random.randn(2000) * 10
-y = np.random.randn(2000) * 10
-z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+    x = np.random.randn(2000) * 10
+    y = np.random.randn(2000) * 10
+    z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
 
-plt.scatter(x, y, c=z)
-plt.colorbar(label="elevation (m)")
-plt.xlabel('x coordinate (m)')
-plt.ylabel('y coordinate (m)')
-plt.title("Mountain Elevation")
-plt.show()
+    plt.scatter(x, y, c=z)
+    plt.colorbar(label="elevation (m)")
+    plt.xlabel('x coordinate (m)')
+    plt.ylabel('y coordinate (m)')
+    plt.title("Mountain Elevation")
+    plt.show()
