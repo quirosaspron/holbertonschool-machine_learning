48bf5077cc5c84d626d4a708ebb59efd62d50412
Mateo <mateoquirosaspron@gmail.com>
2024-04-28 13:09:26 -0600

    -

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python3
-"""Plots a stacked bar graph"""
+"""plot a stacked bar graph"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 def bars():
-    "Plots a bar graph"
+    """colums of fruit represent # of fruit per person
+    rows of fruit represent the number of apples, bananas,
+    oranges, and peaches
+    bars width 0.5
+    y-axis ticks every 10 units, range 0-80"""
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
-    rows = ('apples', 'bananas', 'oranges', 'peaches')
-    columns = ('Farrah', 'Fred', 'Felicia')
-    index = columns
-    colors = ('red', 'yellow', '#ff8000', '#ffe5b4')
-    n_rows = len(fruit)
-    bar_width = 0.5
-    y_offset = np.zeros(len(columns))
 
+    plt.bar([1, 2, 3], fruit[0], color='red', width=0.5,
+            label='apples')
+    plt.bar([1, 2, 3], fruit[1], color='yellow', width=0.5,
+            bottom=fruit[0], label='bananas')
+    plt.bar([1, 2, 3], fruit[2], color='#ff8000', width=0.5,
+            bottom=fruit[0] + fruit[1], label='oranges')
+    plt.bar([1, 2, 3], fruit[3], color='#ffe5b4', width=0.5,
+            bottom=fruit[0] + fruit[1] + fruit[2], label='peaches')
 
-   for row in range(n_rows):
-       plt.bar(index, fruit[row], bar_width, bottom=y_offset,
-              color=colors[row], label=rows[row])
-       y_offset = y_offset + fruit[row]
-
-
-    plt.legend()
-    plt.yticks(np.arange(0, 90, 10))
     plt.ylabel('Quantity of Fruit')
-    plt.title("Number of Fruit per Person")
+    plt.title('Number of Fruit per Person')
+    plt.legend()
+    plt.xticks([1, 2, 3], ['Farrah', 'Fred', 'Felicia'])
+    plt.yticks(range(0, 81, 10))
+    plt.ylim(0, 80)
     plt.show()
