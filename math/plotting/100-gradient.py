ef05323d72dcc010d4ce3ba9933062ceaaddbd40
Mateo <mateoquirosaspron@gmail.com>
2024-04-28 13:15:18 -0600

    -

--- math/plotting/100-gradient.py
+++ math/plotting/100-gradient.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+""" plot mountain elevation as a scatter plot with colorbar """
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,11 +9,9 @@ x = np.random.randn(2000) * 10
 y = np.random.randn(2000) * 10
 z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
 
-# your code here
-
-plt.title("Mountain Elevation")
-plt.xlabel("x coordinate (m)")
-plt.ylabel("y coordinate (m)")
-plt.scatter(x, y, s=40, c=z)
+plt.scatter(x, y, c=z)
 plt.colorbar(label="elevation (m)")
+plt.xlabel('x coordinate (m)')
+plt.ylabel('y coordinate (m)')
+plt.title("Mountain Elevation")
 plt.show()
