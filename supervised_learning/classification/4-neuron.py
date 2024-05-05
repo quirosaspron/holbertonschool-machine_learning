10eb1f8a0d20d462594f205c241131b4a955ba03
Mateo <mateoquirosaspron@gmail.com>
2024-05-05 13:20:21 -0600

    -

---
+++ supervised_learning/classification/4-neuron.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Contains a single neuron class performing binary classification"""
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron"""
+    def __init__(self, nx):
+        """Initializes the weights, bias and activated output"""
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Gets the weight attribute"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Gets the bias attribute"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Gets the activated output attribute"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Peforms forward propagation using the sigmoid activation"""
+        op = np.dot(self.__W, X) + self.__b
+        self.__A = 1/(1 + np.exp(-op))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculates the cost function,
+            Y is correct value, A is the activated output"""
+        m = Y.shape[1]
+        cost = -1/m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """Evaluates the neuron's predictions"""
+        self.forward_prop(X)
+        cost = self.cost(Y, self.__A)
+        predictions = np.zeros((1, Y.shape[1]))
+        for i in range(0, self.__A.shape[1]):
+            if self.__A[0][i] >= 0.5:
+                predictions[0][i] = 1
+        return predictions, cost
