d18516ccb6a4f226f53307277c4d1241ffa96e8a
Mateo <mateoquirosaspron@gmail.com>
2024-05-06 17:23:27 -0600

    -

---
+++ supervised_learning/classification/10-neural_network.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""Builds a neural network"""
+import numpy as np
+
+
+class NeuralNetwork():
+    """Neural network class"""
+    def __init__(self, nx, nodes):
+        """Initializes the neural net"""
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if not isinstance(nodes, int):
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Gets the weight 1 attribute"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Gets the bias 1 attribute"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Gets the hidden activated output"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Gets the weight 2 attribute"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Gets the bias 2 attribute"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Gets the final activated output"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation"""
+        activation_1 = np.dot(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1+np.exp(-activation_1))
+        activation_2 = np.dot(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1+np.exp(-activation_2))
+        return self.__A1, self.__A2
