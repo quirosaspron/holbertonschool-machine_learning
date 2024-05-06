76efb49062d921d44caf5840bc813bf5b86b1381
Mateo <mateoquirosaspron@gmail.com>
2024-05-06 16:51:19 -0600

    -

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Builds a neural network"""
+import numpy as np
+
+
+class NeuralNetwork():
+    """Neural network class"""
+    def __init__(self, nx, nodes):
+        """Initializes the neural net"""
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if not isinstance(nodes, int):
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+        self.W1 = np.random.normal(size=(1, nx))
+        self.b1 = np.zeros((1, nx))
+        self.A1 = 0
+        self.W2 = np.random.normal(size=(1, nx))
+        self.b2 = 0
+        self.A2 = 0
