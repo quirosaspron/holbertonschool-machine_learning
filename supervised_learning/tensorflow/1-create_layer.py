c9a98679c81da34f1b4ddcf6f9ad7198a0aaf5a0
Mateo <mateoquirosaspron@gmail.com>
2024-05-12 14:23:29 -0600

    -

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -6,6 +6,6 @@ import tensorflow.compat.v1 as tf
 def create_layer(prev, n, activation):
     """ Returns the tensor output of a layer """
     init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    layer =  tf.layers.Dense(units=n, activation = activation,
-        kernel_initializer = init, name = 'layer')
+    layer = tf.layers.Dense(units=n, activation=activation
+                            kernel_initializer=init, name='layer')
     return layer(prev)
