02fdb5eeef44f22013b1d971780e494ac72152e8
Mateo <mateoquirosaspron@gmail.com>
2024-05-18 18:15:12 -0600

    -

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -10,8 +10,7 @@ def create_mini_batches(X, Y, batch_size):
     batches = []
     m = X.shape[0]
     for i in range(0, m, batch_size):
-            batch_X = X[i:i+batch_size]
-            batch_Y = Y[i:i+batch_size]
-            batches.append((batch_X, batch_Y))
-      
+        batch_X = X[i:i+batch_size]
+        batch_Y = Y[i:i+batch_size]
+        batches.append((batch_X, batch_Y))
     return batches
