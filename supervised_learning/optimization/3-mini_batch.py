9229b9e650860125f01151f4f48aed838fea2cbc
Mateo <mateoquirosaspron@gmail.com>
2024-05-18 18:13:20 -0600

    -

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """Creates mini-batches to be used for training"""
 import numpy as np
+shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
 def create_mini_batches(X, Y, batch_size):
     """Returns: list of mini-batches"""
+    X, Y = shuffle_data(X, Y)
     batches = []
     m = X.shape[0]
     for i in range(0, m, batch_size):
