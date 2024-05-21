6189d58fac1f30e392ea169dfe8015b7cdd40e69
Mateo <mateoquirosaspron@gmail.com>
2024-05-21 11:39:35 -0600

    -

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Calculates F1 score for each class in a confusion matrix"""
 import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
