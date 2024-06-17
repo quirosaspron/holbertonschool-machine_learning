8a1041e59e6297e5f628a48ec068d28bb7d5512e
Mateo <mateoquirosaspron@gmail.com>
2024-06-16 21:21:21 -0600

    -

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -28,19 +28,20 @@ def projection_block(A_prev, filters, s=2):
     tensor
         The activated output of the projection block.
     """
+    F11, F3, F12 = filters
     init = K.initializers.HeNormal(seed=0)
-    layer_1 = K.layers.Conv2D(filters[0], (1, 1), strides=(s, s),
+    layer_1 = K.layers.Conv2D(filters=F11, (1, 1), strides=(s, s),
                               padding='same',
                               kernel_initializer=init)(A_prev)
     layer_1 = K.layers.BatchNormalization(axis=-1)(layer_1)
     layer_1 = K.layers.Activation('relu')(layer_1)
 
-    layer_2 = K.layers.Conv2D(filters[1], (3, 3), padding='same',
+    layer_2 = K.layers.Conv2D(filters=F3, (3, 3), padding='same',
                               kernel_initializer=init)(layer_1)
     layer_2 = K.layers.BatchNormalization(axis=-1)(layer_2)
     layer_2 = K.layers.Activation('relu')(layer_2)
 
-    layer_3 = K.layers.Conv2D(filters[2], (1, 1), padding='same',
+    layer_3 = K.layers.Conv2D(filters=F12, (1, 1), padding='same',
                               kernel_initializer=init)(layer_2)
     layer_3 = K.layers.BatchNormalization(axis=-1)(layer_3)
     shortcut_layer = K.layers.Conv2D(filters[2], (1, 1), strides=(s, s),
