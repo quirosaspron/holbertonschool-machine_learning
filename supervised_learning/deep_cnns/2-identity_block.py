bc7ea5f5efd95645ae01a5aee4af4eab14b22ef0
Mateo <mateoquirosaspron@gmail.com>
2024-06-16 21:21:21 -0600

    -

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -25,17 +25,18 @@ def identity_block(A_prev, filters):
         The activated output of the identity block.
     """
     init = K.initializers.HeNormal(seed=0)
-    layer_1 = K.layers.Conv2D(filters[0], (1, 1), padding='same',
+    F11, F3, F12 = filters
+    layer_1 = K.layers.Conv2D(filters=F11, (1, 1), padding='same',
                               kernel_initializer=init)(A_prev)
     layer_1 = K.layers.BatchNormalization(axis=-1)(layer_1)
     layer_1 = K.layers.Activation('relu')(layer_1)
 
-    layer_2 = K.layers.Conv2D(filters[1], (3, 3), padding='same',
+    layer_2 = K.layers.Conv2D(filters=F3, (3, 3), padding='same',
                               kernel_initializer=init)(layer_1)
     layer_2 = K.layers.BatchNormalization(axis=-1)(layer_2)
     layer_2 = K.layers.Activation('relu')(layer_2)
 
-    layer_3 = K.layers.Conv2D(filters[2], (1, 1), padding='same',
+    layer_3 = K.layers.Conv2D(filters=F12, (1, 1), padding='same',
                               kernel_initializer=init)(layer_2)
     layer_3 = K.layers.BatchNormalization(axis=-1)(layer_3)
     layer_3 = K.layers.Activation('relu')(layer_3 + A_prev)
