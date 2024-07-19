5d41053929d620a063f8b7de74dd8bcacfbd74c6
Mateo <mateoquirosaspron@gmail.com>
2024-07-18 20:03:34 -0600

    -

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -12,7 +12,7 @@ class Binomial:
         if data is None:
             if not n > 0:
                 raise ValueError('n must be a positive value')
-            if not (0 <= p <= 1):
+            if not (0 < p < 1):
                 raise ValueError('p must be greater than 0 and less than 1')
             else:
                 self.n = int(n)
