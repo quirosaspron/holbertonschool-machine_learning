b7aa61446b00b83f0fb90093d44c27dd4b5dd655
Mateo <mateoquirosaspron@gmail.com>
2024-07-23 21:09:44 -0600

    -

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -29,11 +29,14 @@ that is greater than or equal to 0')
     if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
         raise TypeError('Pr must be a numpy.ndarray with the \
 same shape as P')
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
     for a, b in zip(P, Pr):
         if not (0 <= a <= 1):
             raise ValueError('All values in P must be in the range [0, 1]')
         if not (0 <= b <= 1):
             raise ValueError('All values in Pr must be in the range [0, 1]')
+
     # Get likelihood
     factorial = np.math.factorial
     fact_coefficient = factorial(n) / (factorial(n - x) * factorial(x))
