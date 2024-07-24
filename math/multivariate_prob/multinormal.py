a18a708d119814159925550ac7dde15e7b9bba9f
Mateo <mateoquirosaspron@gmail.com>
2024-07-24 12:33:35 -0600

    -

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -20,3 +20,22 @@ multiple data points')
         self.mean = mean
         cov = np.matmul(data-mean, data.T-mean.T)/(data.shape[1]-1)
         self.cov = cov
+
+    def pdf(self, x):
+        """Calculates the PDF at a data point"""
+
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.cov.shape[0]
+        if len(x.shape) != 2:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        test_d, one = x.shape
+        if test_d != d or one != 1:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        pdf = 1.0 / np.sqrt(((2 * np.pi) ** d) * det)
+        mult = np.matmul(np.matmul((x - self.mean).T, inv), (x - self.mean))
+        pdf *= np.exp(-0.5 * mult)
+        pdf = pdf[0][0]
+        return pdf
