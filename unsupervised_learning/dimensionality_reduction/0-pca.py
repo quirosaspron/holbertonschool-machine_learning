6f89a82fb4983c50397d24a95e842cb1921e5d89
Mateo <mateoquirosaspron@gmail.com>
2024-07-28 17:05:32 -0600

    -

---
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+PCA on a dataset
+"""
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """
+    Function that performs PCA on a dataset
+    """
+    # Calculate the SVD of input data
+    U, S, V = np.linalg.svd(X, full_matrices=False)
+
+    # Calculate the cumulative sum of the variance ratio
+    var_ratio = np.cumsum(S**2) / np.sum(S**2)
+
+    # Determine the number of components to keep
+    nb_comp = np.argmax(var_ratio >= var) + 1
+
+    # Select first nb_comp components
+    W = V[:nb_comp + 1].T
+
+    return W
