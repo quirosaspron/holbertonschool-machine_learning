b11556624de804f71da822d230604308fe3d33a7
Mateo <mateoquirosaspron@gmail.com>
2024-08-01 21:06:50 -0600

    -

---
+++ unsupervised_learning/clustering/0-initialize.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import numpy as np
+"K-means clustering"
+
+
+def initialize(X, k):
+    """initializes cluster centroids for K-meansi
+       X: numpy.ndarray containing the dataset
+       k: number of clusters"""
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if type(k) != int or k <= 0:
+        return None
+
+    n, d = X.shape
+    low = np.min(X, axis=0)
+    high = np.max(X, axis=0)
+    centroids = np.random.uniform(low, high, size=(k, d))
+    return centroids
