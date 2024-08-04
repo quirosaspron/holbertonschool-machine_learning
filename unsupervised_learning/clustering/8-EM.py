f68c8e1f7aad2ab5ba9f7dc8db69963952d5d4ca
Mateo <mateoquirosaspron@gmail.com>
2024-08-03 22:37:01 -0600

    -

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -8,25 +8,37 @@ maximization = __import__('7-maximization').maximization
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """
-    combition of  task expectation and optimization the achive GMM
+    Performs the expectation maximization for a GMM.
     """
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None, None, None, None, None
+    if not isinstance(k, int) or k <= 0:
+        return None, None, None, None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None, None, None, None
+    if not isinstance(tol, float) or tol < 0:
+        return None, None, None, None, None
+    if not isinstance(verbose, bool):
+        return None, None, None, None, None
 
     pi, m, S = initialize(X, k)
-    L = 0.0000000
+    g, l = expectation(X, pi, m, S)
+
     for i in range(iterations):
-        l_last = L
-        g, L = expectation(X, pi, m, S)
+        pi, m, S = maximization(X, g)
+        g, new_l = expectation(X, pi, m, S)
 
-        if verbose and (i % 10 == 0):
-            print(f"Log Likelihood after {i} iterations: {L:.5f}")
+        if verbose and i % 10 == 0:
+            print(f'Log Likelihood after {i} iterations: {l:.5f}')
 
-        if abs(L - l_last) < tol:
+        if abs(new_l - l) <= tol:
             if verbose:
-                print(f"Log Likelihood after {i} iterations: {L:.5f}")
+                print(f'Log Likelihood after {i} iterations: {new_l:.5f}')
             break
 
-        pi, m, S = maximization(X, g)
+        l = new_l
 
-    g, L = expectation(X, pi, m, S)
+    if verbose:
+        print(f'Log Likelihood after {i} iterations: {l:.5f}')
 
-    return pi, m, S, g, L
+    return pi, m, S, g, l
