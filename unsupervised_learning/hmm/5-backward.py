e003bc6a380e1099f183705c11fd935539001f51
Mateo <mateoquirosaspron@gmail.com>
2024-08-11 11:25:42 -0600

    -

--- unsupervised_learning/hmm/5-backward.py
+++ unsupervised_learning/hmm/5-backward.py
@@ -2,63 +2,26 @@
 """
 Defines function that performs the backward algorithm for a Hidden Markov Model
 """
-
-
 import numpy as np
 
 
 def backward(Observation, Emission, Transition, Initial):
-    """
-    Performs the backward algorithm for a Hidden Markov Model
+    '''
+    Input: Initial distribution v; Transition probability P; Emission
+    probability E;
+    Observations from HMM: Obers (Integer list with outcomes).
+    Output: (backward probabilities for all the time steps, likelihood value
+    based on 'Obser')
+    '''
+    n = len(Observation)
+    num_S = Transition.shape[0]
+    b = np.empty([num_S, n], dtype='float')
+    # Base case
+    b[:, n-1] = 1
+    # Recursive case
+    for t in reversed(range(n-1)):
+        b[:, t] = np.dot(Transition, np.multiply(Emission[:, Observation[t+1]],
+                                                 b[:, t+1]))
 
-    parameters:
-        Observation [numpy.ndarray of shape (T,)]:
-            contains the index of the observation
-            T: number of observations
-        Emission [numpy.ndarray of shape (N, M)]:
-            contains the emission probability of a specific observation
-                given a hidden state
-            N: number of hidden states
-            M: number of all possible observations
-        Transition [2D numpy.ndarray of shape (N, N)]:
-            contains the transition probabilities
-            Transition[i, j] is the probabilitiy of transitioning
-                from the hidden state i to j
-        Initial [numpy.ndarray of shape (N, 1)]:
-            contains the probability of starting in a particular hidden state
-
-    returns:
-        P, B:
-            P [float]:
-                the likelihood of the observations given the model
-            B [a numpy.ndarray of shape (N. T)]:
-                contains the backward path probabilities
-                B[i, j] is the probability of generating future observations
-                    from hidden state i at time j
-        or None, None on failure
-    """
-    # check that Observation is the correct type and dimension
-    if type(Observation) is not np.ndarray or len(Observation.shape) < 1:
-        return None, None
-    # save T from Observation's shape
-    T = Observation.shape[0]
-    # check that Emission is the correct type and dimension
-    if type(Emission) is not np.ndarray or len(Emission.shape) != 2:
-        return None, None
-    # save N and M from Emission's shape
-    N, M = Emission.shape
-    # check that Transition is the correct type and dimension
-    if type(Transition) is not np.ndarray or len(Transition.shape) != 2:
-        return None, None
-    # check that Transition's dimensions match N from Emission
-    N_check1, N_check2 = Transition.shape
-    if N_check1 != N or N_check2 != N:
-        return None, None
-    # check that Initial is the correct type and dimension
-    if type(Initial) is not np.ndarray or len(Initial.shape) != 2:
-        return None, None
-    # check that Initial's dimensions match (N, 1)
-    N_check1, one = Initial.shape
-    if N_check1 != N or one != 1:
-        return None, None
-    return None, None
+    return (np.dot(Initial[:, 0], np.multiply(Emission[:, Observation[0]],
+                                              b[:, 0])), b)
