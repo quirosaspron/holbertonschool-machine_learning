c63cbe1af3f378865e22597700c86149d79286d1
Mateo <mateoquirosaspron@gmail.com>
2024-08-11 11:23:01 -0600

    -

--- unsupervised_learning/hmm/4-viterbi.py
+++ unsupervised_learning/hmm/4-viterbi.py
@@ -3,62 +3,41 @@
 Defines function that calculates that most likely sequence of hidden states for
 the Hidden Markov Model
 """
-
-
 import numpy as np
 
 
 def viterbi(Observation, Emission, Transition, Initial):
-    """
-    Calculates the most likely sequence of hidden states
-    for the Hidden Markov Model
+    '''
+    Input: Initial distribution v; Transition probability P;
+    Emission probability E;
+    Observations from HMM: Obers (Integer list with outcomes).
+    Output: (the most likely sequence of states, maximal joint probability)
+    '''
 
-    parameters:
-        Observation [numpy.ndarray of shape (T,)]:
-            contains the index of the observation
-            T: number of observations
-        Emission [numpy.ndarray of shape (N, M)]:
-            contains the emission probability of a specific observation
-                given a hidden state
-            N: number of hidden states
-            M: number of all possible observations
-        Transition [2D numpy.ndarray of shape (N, N)]:
-            contains the transition probabilities
-            Transition[i, j] is the probabilitiy of transitioning
-                from the hidden state i to j
-        Initial [numpy.ndarray of shape (N, 1)]:
-            contains the probability of starting in a particular hidden state
+    n = len(Observation)
+    num_S = Transition.shape[0]
+    d = np.empty([num_S, n], dtype='float')
+    f = np.empty([num_S, n-1], dtype=int)    # Matrix for backtracking
 
-    returns:
-        path, P:
-            path [list of length T]:
-                contains the most likely sequence of hidden states
-            P [float]:
-                the probability of obtaining the path sequence
-        or None, None on failure
-    """
-    # check that Observation is the correct type and dimension
-    if type(Observation) is not np.ndarray or len(Observation.shape) < 1:
-        return None, None
-    # save T from Observation's shape
-    T = Observation.shape[0]
-    # check that Emission is the correct type and dimension
-    if type(Emission) is not np.ndarray or len(Emission.shape) != 2:
-        return None, None
-    # save N and M from Emission's shape
-    N, M = Emission.shape
-    # check that Transition is the correct type and dimension
-    if type(Transition) is not np.ndarray or len(Transition.shape) != 2:
-        return None, None
-    # check that Transition's dimensions match N from Emission
-    N_check1, N_check2 = Transition.shape
-    if N_check1 != N or N_check2 != N:
-        return None, None
-    # check that Initial is the correct type and dimension
-    if type(Initial) is not np.ndarray or len(Initial.shape) != 2:
-        return None, None
-    # check that Initial's dimensions match (N, 1)
-    N_check1, one = Initial.shape
-    if N_check1 != N or one != 1:
-        return None, None
-    return None, None
+    # Base case
+    d[:, 0] = np.multiply(Initial[:, 0], Emission[:, Observation[0]])
+
+    # Recursive case
+    for t in range(1, n):         # NUMBER OF STEP
+        for i in range(num_S):    # NUMBER OF STATES
+            temp_vec = np.multiply(d[:, t-1], Transition[:, i])
+            f[i, t-1] = np.argmax(temp_vec)
+            d[i, t] = Emission[i, Observation[t]] * np.max(temp_vec)
+
+    p_star = np.max(d[:, n-1])    # max sequence
+    most_lik = []
+
+    # The last element of the most likely sequence of states
+    x = np.argmax(d[:, n-1])
+    most_lik.append(int(x))
+    # Backtracking
+    for t in reversed(range(n-1)):
+        x = f[x, t]
+        most_lik.append(int(x))
+
+    return most_lik[::-1], p_star
