506f94a0185c7c9f2812fd5a08626ef9e74c570f
Mateo <mateoquirosaspron@gmail.com>
2024-08-24 19:12:11 -0600

    -

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -17,11 +17,11 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     decoder.add(keras.layers.Input(shape=(latent_dims, )))
     for i in reversed(hidden_layers):
         decoder.add(keras.layers.Dense(i, activation='relu'))
-    decoder.add(keras.layers.Dense(input_dims, activation='relu'))
+    decoder.add(keras.layers.Dense(input_dims, activation='sigmoid'))
 
     autoencoder = keras.models.Sequential()
     autoencoder.add(encoder)
     autoencoder.add(decoder)
-    autoencoder.compile(optimizer='adam', loss='categorical_crossentropy')
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
 
     return encoder, decoder, autoencoder
