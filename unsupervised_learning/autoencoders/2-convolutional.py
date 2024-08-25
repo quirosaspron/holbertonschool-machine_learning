bf4706d0bd931ca10d58e6e5aa8fb116a5f0aec7
Mateo <mateoquirosaspron@gmail.com>
2024-08-24 21:57:42 -0600

    -

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -14,30 +14,35 @@ def autoencoder(input_dims, filters, latent_dims):
     encoder = input_layer
     for i in filters:
         encoder = keras.layers.Conv2D(i, (3, 3),
-                                padding='same', activation='relu')(encoder)
-        encoder = keras.layers.MaxPool2D(pool_size=(2, 2), padding='same')(encoder)
+                                      padding='same',
+                                      activation='relu')(encoder)
+        encoder = keras.layers.MaxPool2D((2, 2),
+                                         padding='same')(encoder)
 
     encoder = keras.Model(inputs=input_layer, outputs=encoder)
 
-    #Decoder
+    # Decoder
     input_layer = keras.layers.Input(shape=latent_dims)
 
     decoder = input_layer
     for i in reversed(filters[1:]):
         decoder = keras.layers.Conv2D(i, (3, 3),
-                                padding='same', activation='relu')(decoder)
+                                      padding='same',
+                                      activation='relu')(decoder)
         decoder = keras.layers.UpSampling2D(size=(2, 2))(decoder)
 
     decoder = keras.layers.Conv2D(filters[0], (3, 3),
-                            padding='valid', activation='relu')(decoder)
+                                  padding='valid',
+                                  activation='relu')(decoder)
     decoder = keras.layers.UpSampling2D(size=(2, 2))(decoder)
 
     decoder = keras.layers.Conv2D(input_dims[-1], (3, 3),
-                            padding='same', activation='sigmoid')(decoder)
+                                  padding='same',
+                                  activation='sigmoid')(decoder)
 
     decoder = keras.models.Model(inputs=input_layer, outputs=decoder)
 
-    #Autoencoder
+    # Autoencoder
 
     encoder_input = keras.layers.Input(shape=input_dims)
     encoded_output = encoder(encoder_input)
