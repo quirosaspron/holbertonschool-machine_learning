f7924a281301d1c01b160a142954fe2be31585a2
Mateo <mateoquirosaspron@gmail.com>
2024-09-20 22:15:52 -0600

    -

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Creates, builds and trains a gensim word2vec model"""
-from gensim.models import Word2Vec
+import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5,
                    window=5, negative=5, cbow=True, epochs=5,
                    seed=0, workers=1):
-    """sentences: list of sentences to be trained on
+    """
+       sentences: list of sentences to be trained on
        vector_size: dimensionality of embedding layer
        min_count: minimum number of occurrences of a
                   word for use in training
@@ -17,18 +18,23 @@ def word2vec_model(sentences, vector_size=100, min_count=5,
        epochs: number of iterations to train over
        seed: seed for the random number generator
        workers: number of worker threads to train the model
-       Returns: the trained model """
+       Returns: the trained model
+     """
 
     sg = 0 if cbow else 1
 
-    model = Word2Vec(sentences=sentences,
-                     vector_size=vector_size,
-                     seed=seed,
-                     sg=sg,
-                     negative=negative,
-                     window=window,
-                     min_count=min_count,
-                     workers=workers)
+    model = gensim.models.Word2Vec(sentences=sentences,
+                                   vector_size=vector_size,
+                                   seed=seed,
+                                   sg=sg,
+                                   negative=negative,
+                                   window=window,
+                                   min_count=min_count,
+                                   workers=workers)
+
+    # build the vocabulary
+    # this hepls to create the one-hot encoding of the words
+    model.build_vocab(sentences)
 
     model.train(sentences,
                 total_examples=model.corpus_count,
