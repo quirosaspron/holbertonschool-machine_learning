9d8d1963a840298abcb7d9419d1116052c2e258e
Mateo <mateoquirosaspron@gmail.com>
2024-10-03 22:28:23 -0600

    -

--- supervised_learning/transformer_apps/3-dataset.py
+++ supervised_learning/transformer_apps/3-dataset.py
@@ -2,11 +2,12 @@
 """Loads and preps a dataset for machine translation"""
 import tensorflow_datasets as tfds
 import transformers
+import tensorflow as tf
 
 
 class Dataset:
     """Dataset class"""
-    def __init__(self):
+    def __init__(self, batch_size, max_len):
         """Class constructor"""
         self.data_train = tfds.load(
             'ted_hrlr_translate/pt_to_en',
@@ -21,9 +22,23 @@ class Dataset:
         self.tokenizer_pt, self.tokenizer_en = (
             self.tokenize_dataset(self.data_train))
 
-        # Tokenize and encode the datasets using the `tf_encode` method
+        # Update data_train: Filter, cache, shuffle, split, and prefetch
         self.data_train = self.data_train.map(self.tf_encode)
+        self.data_train = self.data_train.filter(lambda pt, en: tf.logical_and(
+            tf.size(pt) <= max_len, tf.size(en) <= max_len))
+        self.data_train = self.data_train.cache()
+        self.data_train = self.data_train.shuffle(20000)
+        self.data_train = self.data_train.padded_batch(
+            batch_size, padded_shapes=([None], [None]))
+        self.data_train = self.data_train.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+
+        # Update data_valid: Filter and split
         self.data_valid = self.data_valid.map(self.tf_encode)
+        self.data_valid = self.data_valid.filter(lambda pt, en: tf.logical_and(
+            tf.size(pt) <= max_len, tf.size(en) <= max_len))
+        self.data_valid = self.data_valid.padded_batch(
+            batch_size, padded_shapes=([None], [None]))
 
     def tokenize_dataset(self, data):
         """ Creates sub-word tokenizers
@@ -75,5 +90,6 @@ class Dataset:
         """Acts as a tensorflow wrapper for the encode instance method"""
         tf_encoder = tf.py_function(func=self.encode, inp=[pt, en],
                                     Tout=[tf.int64, tf.int64])
-
-        return pt_tensor, en_tensor
+        pt_tensor = tf.ensure_shape(tf_encoder[0], [None])
+        en_tensor = tf.ensure_shape(tf_encoder[1], [None])
+        return pt_tensor, en_tensor 
