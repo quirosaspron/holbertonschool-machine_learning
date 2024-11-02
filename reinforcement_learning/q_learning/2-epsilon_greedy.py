9151c6afe7cbdd056e8712746d3379597c88bb3d
Mateo <mateoquirosaspron@gmail.com>
2024-11-02 14:34:19 -0600

    -

---
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""uses epsilon-greedy to determine the next action"""
+import numpy as np
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """"
+    Q: np.ndarray q-table
+    state: current state
+    epsilon: epsilon to use for the calculation
+    Returns: the next action index
+    """
+    p = np.random.uniform()
+    if p > epsilon:  # Exploitation
+        action = np.argmax(Q[state, :])
+    else:  # Exploration
+        action = np.random.randint(0, 3)
+
+    return action
